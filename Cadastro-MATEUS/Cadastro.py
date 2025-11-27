9750ea3876dee79968281ba9a7a98b86da984e5e
CookieBR2004 <cookiebr2004@gmail.com>
2025-11-27 01:11:34 -0300

    Add files via upload

---
+++ Cadastro-MATEUS/Cadastro.py
@@ -0,0 +1,79 @@
+# Sistema simples de cadastro de clientes
+# Criado por Mateus - Projeto Integrador 2025
+
+# Python v3.14
+
+Cadastro = []
+
+def CadastrarCliente():
+    Nome = input("Nome do cliente: ")
+    Email = input("Email do cliente: ")
+    Phone = input("Telefone do cliente: ")
+
+    while True: 
+        print("--- OPÇÕES DE PLANO ---")
+        print("1 - Mensal")
+        print("2 - Anual")
+        print("3 - Plano Black")
+
+        Plano1 = input("Escolha o número do Plano do cliente: ")
+
+        if Plano1 == "1":
+            Plano = "Mensal R$49,99/mês"
+            break
+        elif Plano1 == "2":
+            Plano = "Anual R$599,99/anual"
+            break
+        elif Plano1 == "3":
+            Plano = "Plano Black R$199,99/anual"
+            break
+        else:
+            print("❌ Opção de plano inválida! Tente novamente.")
+
+    Cadastro.append({
+        "Nome": Nome,
+        "Email": Email,
+        "Phone": Phone,
+        "Plano": Plano
+    })
+
+    print("✔️   Cliente cadastrado com sucesso!")
+
+def Listar():
+    if not Cadastro:
+        print("Nenhum cliente cadastrado.")
+        return
+
+    print(" ")
+    print(" ")
+    print(" ")
+    print("--- LISTA DE CLIENTES ---")
+    for A in Cadastro:
+        print(" ")
+        print(f"👉   {A['Nome']} - {A['Email']} - {A['Phone']} - {A['Plano']}")
+
+def menu():
+    while True:
+        print(" ")
+        print(" ")
+        print(" ")
+        print("------------------------------------------------------")
+        print("1 - Cadastrar cliente / 2 - Listar clientes / 3 - Sair")
+        print(" ")
+
+        B = input("Escolha: ")
+
+        if B == "1":
+            CadastrarCliente()
+        elif B == "2":
+            Listar()
+        elif B == "3":
+            print("Saindo...")
+            print("💠 Criado por Mateus")
+            print("💠 RA: 5425735")
+            print("💠 Curso: Eng. de Software")
+            break
+        else:
+            print("Opção inválida!")
+
+menu()
